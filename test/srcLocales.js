d409014ece1bc1511ead7d2d119ef980c3aced68
Josh Crowther <jshcrowthe@gmail.com>
2016-10-29 17:01:52 -0600

    Refactor WCI18n to support a native WC use case

---
+++ test/srcLocales.js
@@ -0,0 +1,16 @@
+// This is a nasty global test hack. Super bad but it works
+var srcLocales = {
+  "en":{
+    "hello-msg":"Hello!",
+    "fancy-msg":"This is a {type} {thing}"
+  },
+  "ko":{
+    "hello-msg":"안녕하세요!"
+  },
+  "fr":{
+    "hello-msg":"Bonjour!"
+  },
+  "de":{
+    "hello-msg":"Gutentag!"
+  }
+};
